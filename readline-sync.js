9eb076d3895f131d6aa8ee5761cfae46113e889e
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-22 15:07:44 -0300

    Exercises 3.0

--- readline-sync.js
+++ readline-sync.js
@@ -1,4 +1,4 @@
-import entradaDados from 'readline-sync'
+/*import entradaDados from 'readline-sync'
 
 let nome =entradaDados.question('Digite seu nome: ')
 console.log("Olá "+nome)
@@ -9,3 +9,16 @@ let y = entradaDados.question("Digite um segundo valor: ")
 let soma = Number(x) + Number(y)
 
 console.log("Resutado da soma dos valores é: "+soma)
+
+let a = entradaDados.question("Informe o valor de a: ");
+let b = entradaDados.question("Informe o valor de b: ");
+
+let subtracao = a - b;
+
+console.log("\nResultado: "+subtracao);*/
+
+import rl_sync from 'readlineSync';
+
+let nome = rl_sync.question("Informe seu nome: ");
+
+console.log("Olá, "+nome+"!");
