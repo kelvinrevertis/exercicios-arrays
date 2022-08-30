a6b5b3ae545b99feb518987492cbd0d70a0410d0
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-30 20:00:11 -0300

    Exercicios Looping 2.0

--- script.js
+++ script.js
@@ -69,3 +69,123 @@ for ( let contador = 1; contador <= 5; contador++ )
     let valor_parcela = valor_produto / contador;
     console.log("Nº de parcelas: " + contador + " - Valor da parcela: R$" + valor_parcela.toFixed(2));
 }
+
+let funcionarios = [
+    {id: 1, nome: "César", habilitado: false},
+    {id: 2, nome: "Jéssica", habilitado: false},
+    {id: 3, nome: "Renan", habilitado: true},
+    {id: 4, nome: "Marlon", habilitado: false},
+    {id: 5, nome: "Ana", habilitado: false}
+];
+
+let cont = 0;
+let encontrouHabilitado = false;
+
+while ( cont < funcionarios.length ) {
+
+    let funcionario = funcionarios[cont];
+
+    if( funcionario.habilitado == true ){
+        console.log("Funcionário habilitado encontrado: " + funcionario.nome);
+        encontrouHabilitado = true;
+        break;
+    }
+
+    cont++;
+}
+
+if (!encontrouHabilitado) {
+    console.log("Nenhum funcionário habilitado encontrado");
+}
+
+let alunos = [
+    {id: 1, nome: "Bruna", media: 8},
+    {id: 2, nome: "Laura", media: 7},
+    {id: 3, nome: "José", media: 5},
+    {id: 4, nome: "Guilherme", media: 4},
+    {id: 5, nome: "Rafael", media: 10},
+];
+
+let contador = 0;
+
+while ( contador < alunos.length ) {
+
+    let aluno = alunos[contador];
+
+    contador++;
+
+    if ( aluno.media < 6 ) {
+        break;
+    }
+
+    console.log("Id: " + aluno.id);
+    console.log("Nome: " + aluno.nome);
+    console.log("Média: " + aluno.media);
+    console.log("\n");
+
+}
+
+//exemplo for in
+
+let pokemon = [ {nome:"Pikachu", tipo:"Eletrico", habilidade: "Statica", Numero: 25}]
+
+for (let dados in pokemon){
+    console.log(pokemon[dados])
+}
+
+let carro = {nome:"Argo", marca:"Fiat", preco:"50000", ano:2020}
+
+let percentual_desconto = 5
+
+for (let propriedade in carro){
+    if (propriedade == "preco"){
+        let preco = propriedade
+        let desconto = (carro[preco]*percentual_desconto)/100
+        let novo_preco = carro[preco] - desconto
+        console.log("novo preco: R$"+ novo_preco+",00")
+    }else{
+        console.log(propriedade+":"+carro[propriedade])
+    }
+}
+
+let meses= ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+for(let contador = 0; contador <= 11; contador++)
+        {
+
+    console.log(meses[contador]);
+
+    if (meses[contador] == "Junho" ) {
+        break;
+    }
+
+}
+
+
+let notas = [5, 7, 6, 8];
+
+let media = 0;
+
+do {
+
+    console.log(notas[contador]);
+
+    soma = soma + notas[contador];
+
+    contador++;
+
+} while ( contador < notas.length );
+
+media = soma / notas.length;
+
+if( media >= 6 ){
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
+
+let clientes_array = [
+    { id: 1, nome: "Mônica", telefone: "99999-6565" },
+    { id: 2, nome: "Tânia", telefone: "99999-0778" },
+    { id: 3, nome: "Carlos", telefone: "99999-0551" }
+];
